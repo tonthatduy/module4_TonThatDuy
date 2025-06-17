681fc75815c801b0bceb332e35d2647fd1e77f95
Béo Dui <tonthatduy1997@gmail.com>
2025-06-17 17:24:14 +0700

    bai tap

--- bai_tap_1/src/main/java/com/example/bai_tap_1/controller/ExchangeController.java
+++ bai_tap_1/src/main/java/com/example/bai_tap_1/controller/ExchangeController.java
@@ -1,4 +1,24 @@
 package com.example.bai_tap_1.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class ExchangeController {
+    @GetMapping("/home")
+    public String showList() {
+        return "index";
+    }
+
+    @PostMapping("/home")
+    public String convert(Model model, @RequestParam("usd") int usd) {
+        int vnd = usd * 26000;
+        model.addAttribute("usd", usd);
+        model.addAttribute("vnd", vnd);
+        return "index";
+    }
 }
--- bai_tap_1/src/main/webapp/WEB-INF/views/index.jsp
+++ bai_tap_1/src/main/webapp/WEB-INF/views/index.jsp
@@ -7,10 +7,17 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>ExChange Money</h1>
+<form method="post" action="">
+    <p>Tiền USD</p>
+    <input type="number" name="usd" value="${usd}">
+    <p>Tiền Việt Nam</p>
+    <input type="number" name="vnd" value="${vnd}">
+    <button>Chuyển Đổi</button>
+</form>
+</body>
 </html>
