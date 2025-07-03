5d749cfd7c066e0ee9677f2f9f9c2cf97111c34e
Béo Dui <tonthatduy1997@gmail.com>
2025-07-03 16:19:05 +0700

    bai tap i18n

--- demo1/src/main/java/com/example/demo/repository/IBlogRepository.java
+++ demo1/src/main/java/com/example/demo/repository/IBlogRepository.java
@@ -1,11 +1,10 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Blog;
-import com.example.demo.model.Category;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
 
 public interface IBlogRepository extends JpaRepository<Blog, Long> {
     Page<Blog> findBlogByTitleContaining(String title, Pageable pageable);
